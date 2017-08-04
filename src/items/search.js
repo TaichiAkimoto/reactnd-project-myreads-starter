60a10b9d11613d17817a05189a6ce89b2693a43c
TaichiAkimoto <italyitalienitalia@gmail.com>
2017-08-04 22:00:15 +0900

    Add basic query

--- src/items/search.js
+++ src/items/search.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import '../App.css'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from '../BooksAPI'
 
 class Search extends React.Component {
+  state = {
+    result: [],
+    query: "",
+    noquery: true
+  }
+  updateQuery = (query) => {
+    if(query){
+      this.setState({ query: query })
+      BooksAPI.search(query.trim(), 20).then((result) => {
+        if(result.error) {
+          this.setState({ result: [], noquery: true })
+        } else {
+          this.setState({ result, noquery: false })
+        }
+      })
+    } else {
+      this.setState({ query: "", result:[], noquery: true })
+    }
+  }
+
   render() {
+    const { result, query, noquery } = this.state
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -20,12 +42,41 @@ class Search extends React.Component {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input type="text" placeholder="Search by title or author"/>
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={query}
+              onChange={(event) => this.updateQuery(event.target.value)}
+            />
 
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+        {noquery && (
+          <h2>No results found</h2>
+        )}
+          <ol className="books-grid">
+          {result.map((book) => (
+            <li key={book.id}>
+              <div className="book">
+                <div className="book-top">
+                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                  <div className="book-shelf-changer">
+                    <select>
+                      <option value="none" disabled>Move to...</option>
+                      <option value="currentlyReading">Currently Reading</option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                {book.authors && (<div className="book-authors">{book.authors[0]}</div>)}
+              </div>
+            </li>
+          ))}
+          </ol>
         </div>
       </div>
     )
